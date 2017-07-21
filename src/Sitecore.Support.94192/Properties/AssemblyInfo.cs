6a15d52e1fca0d325080119e36394259855cd27e
Dmitry Kolinchuk <dmko@sitecore.net>
2017-07-21 15:42:27 +0300

    Fixes the issue with the stuck horizontal and vertical scrolling when using Device Simulator in Experience Editor

---
+++ src/Sitecore.Support.94192/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94192")]
+[assembly: AssemblyProduct("Sitecore.Support.94192")]
+[assembly: ComVisible(false)]
